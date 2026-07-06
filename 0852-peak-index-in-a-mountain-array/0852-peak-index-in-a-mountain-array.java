41f9009da0073f19cd3d4c1a97cb210da8e60a2c
bhakti-shingadi <149251956+bhakti-shingadi@users.noreply.github.com>
2026-07-06 19:04:24 +0530

    Time: 0 ms (100%), Space: 80.1 MB (88.8%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+         int start=0;
+        int end= arr.length-1;
+        while(start<end){
+            int mid= start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                //you are in dec part, this may be the ans but look for left
+                //this is why end!=mid-1
+                end=mid;
+            }else{
+                //you are in asce part 
+                start=mid+1; // coz we know that mid +1 ele > mid ele
+            }
+        }
+        //in the end start==end and pointing to the largest number coz of 2 checks above 
+        return  start ;
+    }
+}
